eabe1c86c691ebf8ed165d0c22a62f7c4b03e4e8
Colin Seymour <colin@github.com>
2019-08-14 14:33:56 +0100

    Remove debug

--- lib/linguist/repository.rb
+++ lib/linguist/repository.rb
@@ -95,11 +95,7 @@ module Linguist
     def breakdown_by_file
       @file_breakdown ||= begin
         breakdown = Hash.new { |h,k| h[k] = Array.new }
-        p @cache
-        p cache
-
         cache.each do |filename, (language, _)|
-          puts filename.dup.force_encoding("UTF-8").scrub if filename =~ /fixtures/
           breakdown[language] << filename.dup.force_encoding("UTF-8").scrub
         end
         breakdown
